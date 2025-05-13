7f17f2a88f2dfcfd56fd091169eb0f929cee8f5c
Boniatti1 <leonardo.boniattisilva@gmail.com>
2025-05-13 17:25:25 -0300

    feat: configure CA generation script to support CRL and add certificate management functions

--- sistema-seguranca/scripts/gen_certs.sh
+++ sistema-seguranca/scripts/gen_certs.sh
@@ -1,26 +1,51 @@
-CERTS_DIR="../data/nginx/certs"
-mkdir -p "$CERTS_DIR"
+CA_DIR="../data/ssl"
+
+# Criar estrutura de pastas
+mkdir -p "$CA_DIR"/{ca,server,clients}
+touch "$CA_DIR/ca/index.txt"
+echo 1000 > "$CA_DIR/ca/serial"
+echo 1000 > "$CA_DIR/ca/crlnumber"
 
 # Gerar CA
-openssl genrsa -out "$CERTS_DIR/ca.key" 4096
-openssl req -new -x509 -sha256 -days 365 -key "$CERTS_DIR/ca.key" -out "$CERTS_DIR/ca.crt" \
+openssl genrsa -out "$CA_DIR/ca/ca.key" 4096
+chmod 600 "$CA_DIR/ca/ca.key"
+openssl req -new -x509 -days 3650 -key "$CA_DIR/ca/ca.key" -out "$CA_DIR/ca/ca.crt" \
   -subj "/CN=CA-VidaMais/O=Clinica VidaMais/L=Palmital/ST=PR/C=BR"
 
 # Certificado do Servidor
-openssl genrsa -out "$CERTS_DIR/server.key" 4096
-openssl req -new -sha256 -key "$CERTS_DIR/server.key" -out "$CERTS_DIR/server.csr" \
+openssl genrsa -out "$CA_DIR/server/server.key" 4096
+chmod 600 "$CA_DIR/server/server.key"
+openssl req -new -key "$CA_DIR/server/server.key" -out "$CA_DIR/server/server.csr" \
   -subj "/CN=vidamais.top/O=Clinica VidaMais/L=Palmital/ST=PR/C=BR"
-openssl x509 -req -sha256 -days 365 -in "$CERTS_DIR/server.csr" -CA "$CERTS_DIR/ca.crt" -CAkey "$CERTS_DIR/ca.key" -out "$CERTS_DIR/server.crt"
+openssl x509 -req -days 365 -in "$CA_DIR/server/server.csr" \
+  -CA "$CA_DIR/ca/ca.crt" -CAkey "$CA_DIR/ca/ca.key" -CAcreateserial \
+  -out "$CA_DIR/server/server.crt"
+rm "$CA_DIR/server/server.csr"
 
-# Certificado do Cliente (mTLS)
-openssl genrsa -out "$CERTS_DIR/client.key" 4096
-openssl req -new -sha256 -key "$CERTS_DIR/client.key" -out "$CERTS_DIR/client.csr" \
-  -subj "/CN=Computador Clinica 1/O=Clinica VidaMais/L=Palmital/ST=PR/C=BR"
-openssl x509 -req -sha256 -days 365 -in "$CERTS_DIR/client.csr" -CA "$CERTS_DIR/ca.crt" -CAkey "$CERTS_DIR/ca.key" -out "$CERTS_DIR/client.crt"
+# Gerar CRL
+openssl ca -gencrl -out "$CA_DIR/ca/crl.pem" \
+  -keyfile "$CA_DIR/ca/ca.key" -cert "$CA_DIR/ca/ca.crt" \
+  -config <(cat <<EOF
+[ ca ]
+default_ca = CA_default
 
-# Pacote .p12
-openssl pkcs12 -export -out "$CERTS_DIR/client.p12" -inkey "$CERTS_DIR/client.key" -in "$CERTS_DIR/client.crt" -certfile "$CERTS_DIR/ca.crt" -password pass:1234
+[ CA_default ]
+dir               = $CA_DIR/ca
+database          = \$dir/index.txt
+certificate       = \$dir/ca.crt
+private_key       = \$dir/ca.key
+serial            = \$dir/serial
+crlnumber         = \$dir/crlnumber
+default_md        = sha256
+policy            = policy_any
 
-chown -R $(logname):$(logname) "$CERTS_DIR"
-chmod 600 "$CERTS_DIR"/*.key
-chmod 644 "$CERTS_DIR"/*.crt "$CERTS_DIR"/*.csr "$CERTS_DIR"/*.p12
+[ policy_any ]
+countryName             = optional
+stateOrProvinceName     = optional
+organizationName        = optional
+commonName              = supplied
+
+[ crl_ext ]
+authorityKeyIdentifier=keyid:always
+EOF
+)
